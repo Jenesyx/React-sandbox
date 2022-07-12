5ef43814d4f263ed885e4f95caecd00398c64549
Ali Bidkhori <a.bidkhori2004@gmail.com>
2022-07-12 11:09:30 +0200

    0.1.1 Update

--- React-sandbox/src/pages/Homepage/index.jsx
+++ React-sandbox/src/pages/Homepage/index.jsx
@@ -3,15 +3,20 @@ import './style.css'
 import {fakeData} from '../../fakeData/fakeData.js'
 import { useState } from "react"
 import { useEffect } from "react"
+import axios from 'axios'
 const HomePage = () => {
 
     const [productList, setProductList] = useState([])
 
     useEffect(() => {
-        setProductList(fakeData.filter(item => item.rating.rate >= 2))
+        axios.get('https://fakestoreapi.com/products')
+        .then((data) =>{
+            setProductList(data.data)
+        })
+        .catch((err) => console.log(err))
     },[])
 
-    return(
+    return( 
         <div className="d-flex gap-2 flex-wrap justify-content-center">
 
             {productList.map(item => {
